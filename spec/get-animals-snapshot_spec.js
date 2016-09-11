70e6eaf456535fb8120d43d9e87a615e1342eb69
butterfly girl <qmmahbc@163.com>
2016-09-11 12:15:07 +0800

    completed historyDataArr test and judeFormat of id test

--- spec/get-animals-snapshot_spec.js
+++ spec/get-animals-snapshot_spec.js
@@ -1,7 +1,6 @@
-const historyDataToArray = require('../src/get-animals-snapshot');
+const getAnimalsSnapshot = require('../src/get-animals-snapshot');
 
 describe('getSnapshot', ()=> {
-
 
     it('getSnapshot', ()=> {
         const history = `e4e87cb2-8e9a-4749-abb6-26c59344dfee
@@ -16,7 +15,7 @@ cat2 2 3
 dcfa0c7a-5855-4ed2-bc8c-4accae8bd155
 2016/09/02 22:31:02
 cat1 12 8 3 4`;
-        const historyArr = historyDataToArray(history);
+        const historyArr = getAnimalsSnapshot.getHistoryDataArr(history);
         const expectGetAnimalsSnapshot = [
             {
                 id: 'e4e87cb2-8e9a-4749-abb6-26c59344dfee',
@@ -26,17 +25,23 @@ cat1 12 8 3 4`;
             {
                 id: '351055db-33e6-4f9b-bfe1-16f1ac446ac1',
                 date: '2016/09/02 22:30:52',
-                animals: [{ani_id: 'cat1', lastX: 10, lastY: 9, currentX: 2, currentY: -1},
-                    {ani_id: 'cat1', currentX: 2, currentY: 3},]
+                animals: [{ani_id: 'cat1', currentX: 10, currentY: 9, moveX: 2, moveY: -1},
+                    {ani_id: 'cat2', currentX: 2, currentY: 3},]
             },
             {
                 id: 'dcfa0c7a-5855-4ed2-bc8c-4accae8bd155',
                 date: '2016/09/02 22:31:02',
-                animals: [{ani_id: 'cat1', lastX: 12, lastY: 8, currentX: 3, currentY: 4}]
+                animals: [{ani_id: 'cat1', currentX: 12, currentY: 8, moveX: 3, moveY: 4}]
             }
         ];
         expect(historyArr).toEqual(expectGetAnimalsSnapshot);
     });
 
-
+    it('judeFormat',()=>{
+       spyOn(console,'log');
+        const id = 'e4e87cb2-8e9a-4749- abb6-26c59344dfee';
+        const expectResult = 'Invalid format.';
+        getAnimalsSnapshot.judeFormat(id);
+        expect(console.log).toHaveBeenCalledWith(expectResult);
+    });
 });
--- src/get-animals-snapshot.js
+++ src/get-animals-snapshot.js
@@ -1,7 +1,37 @@
-var getSnapshot = ()=> {
+exports.getHistoryDataArr = (history)=> {
 
-    return "ff";
+    const historySplited = history.split("\n\n");
+    return historyArr = historySplited.map(element => {
+        const elementSplited = element.split("\n");
+        const id = elementSplited[0];
+        const date = elementSplited[1];
+        const animals = getAnimalsArr(elementSplited);
+        return {id, date, animals};
+    });
 };
 
+let getAnimalsArr = (elementSplited)=> {
+    const element = elementSplited.splice(2);
+    return animalsArr = element.map(item => {
+        const itemSplited = item.split(' ');
+        const ani_id = itemSplited[0];
+        const currentX = parseInt(itemSplited[1]);
+        const currentY = parseInt(itemSplited[2]);
+        if (itemSplited[3]) {
+            const moveX = parseInt(itemSplited[3]);
+            const moveY = parseInt(itemSplited[4]);
+            return {ani_id, currentX, currentY, moveX, moveY};
+        }
+        return {ani_id, currentX, currentY};
+    });
+};
 
-module.exports = getSnapshot;
+exports.judeFormat = (id) =>{
+  const idSplited = id.split(' ');
+    if(idSplited[1]){
+        console.log('Invalid format.');
+        return false;
+    }
+    return true;
+};
+
